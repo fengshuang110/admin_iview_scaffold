d325729fa25e63f076607b6a1ca3aa4daf1bfd09
fengshuang <945558163@qq.com>
2019-01-23 16:25:23 +0800

    1

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -31,8 +31,7 @@ class AuthServiceProvider extends ServiceProvider
         // the User instance via an API token or any other method necessary.
 
         $this->app['auth']->viaRequest('api', function ($request) {
-             $user = AdminUser::where('id', 1)->first();
-             return $user;
+             
              if(isset($_COOKIE['api_token'])){
                 $token = $_COOKIE['api_token'];
 
